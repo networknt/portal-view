6ef7ba5d79914e4b478e86caee7449cba67d0271
Steve Hu <stevehu@gmail.com>
2020-06-17 22:44:16 -0400

    fixes #4 update deploy-test.sh to deploy the portal view to test1

--- view/deploy-test.sh
+++ view/deploy-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Build the view in in test mode"
 yarn build-dev
-echo "Build completed in build folder, start copying to remote test2 server"
-scp -r ./build/* steve@test2:/home/steve/networknt/light-config-test/light-router/test-portal/lightapi/build
+echo "Build completed in build folder, start copying to remote test1 server"
+ssh test1 "rm -rf ~/light-chain/light-config-test/test1/lightapi/build"
+scp -r ./build test1:/home/steve/light-chain/light-config-test/test1/lightapi
 echo "Copied!"
