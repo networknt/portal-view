b0e24bb101c7376d56b24102a7122c2ca6d5521b
Kelina Hu <kelinahu369@gmail.com>
2020-12-02 16:09:28 -0500

    Change HashRouter to BrowserRouter

--- view/src/App.js
+++ view/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/Layout";
 import Error from "./pages/error";
 
 
 const App = () => {
   return (
-    <HashRouter>
+    <Router>
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
         <Route path="/app" component={Layout} />
         <Route component={Error} />
       </Switch>
-    </HashRouter>
+    </Router>
   );
 }
 
